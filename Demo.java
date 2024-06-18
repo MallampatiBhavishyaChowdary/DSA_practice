bacfb81e217ee65405a144d80062ee8d20b66299
MallampatiBhavishyaChowdary <146431037+MallampatiBhavishyaChowdary@users.noreply.github.com>
2024-06-18 20:48:10 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,13 @@
+/* Basic(to print any statement)
+public class Main{
+    public static void main(String args[]){
+        System.out.println("Bhavishya");
+    }
+}*/
+/* First element in the String array ( Whatever we give in run time will store in string args) all about command line arguments*/
+public class Demo{
+    public static void main(String[] args){
+        //System.out.println(args[0]);//
+        System.out.println(args[1]);
+    }
+}
